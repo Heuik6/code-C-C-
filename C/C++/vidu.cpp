2cfd84c9ebee8d6b4b5f392bb275b9b2c6470a09
Heuik6 <nguyentrunghieu14112006@gmail.com>
2022-11-11 19:41:47 +0700

    add vidu.cpp

--- C/C++/vidu.cpp
+++ C/C++/vidu.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 int main() {
-  for (int i = 0; i < 20; i++) {
-    cout << i << " ";   
+  int x, y, z;
+  cout << "Nhap gia tri cua x: ";
+  cin >> x;
+  cout << "Nhap gia tri cua y: ";
+  cin >> y;
+  cout << "Nhap gia tri cua z: ";
+  cin >> z;
+  int max = x;
+  if (max < y) {
+    max = y;
+  } 
+  if (max < z) {
+    max = z;
   }
-    return 0;
+  cout << "gia tri lon nhat la: " << max;
+  return 0;
 }
