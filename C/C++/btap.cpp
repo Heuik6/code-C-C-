83777c2a19a69717ec24e3ae1dc9e811b504c717
Heuik6 <nguyentrunghieu14112006@gmail.com>
2023-02-04 20:46:30 +0700

    newcode

---
+++ C/C++/btap.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int nNamsinh(0);
+    cout << "Nhap nam sinh cua ban: " ;
+    cin >> nNamsinh;
+    cout << "So tuoi cua ban la " << 2022 - nNamsinh << endl;
+
+    return(0);
+}
