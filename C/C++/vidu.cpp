669ab9a8779f908023ee5fc32f1bd870d49eeddb
Heuik6 <nguyentrunghieu14112006@gmail.com>
2023-02-05 16:48:26 +0700

    clear

--- C/C++/vidu.cpp
+++ C/C++/vidu.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int x, y, z;
-  cout << "Nhap gia tri cua x: ";
-  cin >> x;
-  cout << "Nhap gia tri cua y: ";
-  cin >> y;
-  cout << "Nhap gia tri cua z: ";
-  cin >> z;
-  int max = x;
-  if (max < y) {
-    max = y;
-  } 
-  if (max < z) {
-    max = z;
-  }
-  cout << "gia tri lon nhat la: " << max;
+
+
+
+
   return 0;
 }
+
