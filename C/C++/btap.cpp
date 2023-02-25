4c7dca52dc70f6210ffe62d30b5040fb2c1727e9
Heuik6 <nguyentrunghieu14112006@gmail.com>
2023-02-25 15:03:45 +0700

    fix

--- C/C++/btap.cpp
+++ C/C++/btap.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a[100], n;
+    int a[10], n;
     int s = 0;
-    cout << "Nhap gia tri cua n:";
+    cout << "Nhap gia tri cua n :";
     cin >> n;
-    for (int i = 0; i++; i < n) {
+    cout << "Nhap cac phan tu i:" << endl;
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    for (int i = 0; i++; i < n) {
+    for (int i = 0; i < n; i++) {
         s += a[i];
     }
-    cout << "Tong n so cua mang a la" << s;
+    cout << "Tong n so cua mang a la " << s;
 
     return (0);
 }
